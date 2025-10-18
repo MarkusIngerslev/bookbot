a26b205085243efa4d9ce87f86591428c49bc0f2
Markus Ingerslev <markusolsen2004@gmail.com>
2025-10-18 20:42:50 +0200

    Added ability to read the contents of a book from path and print to terminal

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("greetings boots")
+def main():
+    book_path = "./books/frankenstein.txt"
+    book_text = get_book_text(book_path)
+    print(book_text)
+
+def get_book_text(path):
+    with open(path) as f:
+        file_contents = f.read()
+    return file_contents
+
+main()
