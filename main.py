bab1cf3b766e3121e51170333247af9ca4cae9d3
Markus Ingerslev <markusolsen2004@gmail.com>
2025-10-19 20:56:01 +0200

    Changed functionality to now require to paste the path to given book in focus

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
+import sys
 from stats import get_num_words, get_char_count, sort_char_count
 
 def main():
-    book_path = "books/frankenstein.txt"
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    book_path = sys.argv[1]
     book_text = get_book_text(book_path)
     print(book_text)
 
