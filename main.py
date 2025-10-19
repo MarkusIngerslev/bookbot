046d8bf72f81be65c9de276c160da54b00cc8f8d
Markus Ingerslev <markusolsen2004@gmail.com>
2025-10-19 20:14:17 +0200

    Added a new stats function to count each different charactor in focused book.

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import get_num_words
+from stats import get_num_words, get_char_count
 
 def main():
     book_path = "./books/frankenstein.txt"
@@ -8,6 +8,9 @@ def main():
 def get_book_text(path):
     with open(path) as f:
         file_contents = f.read()
-    return get_num_words(file_contents)
+    word_count = get_num_words(file_contents)
+    char_count = get_char_count(file_contents)
+    print(word_count)
+    print(char_count)
 
 main()
--- stats.py
+++ stats.py
@@ -2,3 +2,13 @@ def get_num_words(text):
     words = text.split()
     num_words = len(words)
     return f"Found {num_words} total words"
+
+def get_char_count(text):
+    char_list = {}
+    text_to_lower = text.lower()
+    for i in range(0, len(text_to_lower)):
+        if text_to_lower[i] in char_list:
+            char_list[text_to_lower[i]] += 1
+        else:
+            char_list[text_to_lower[i]] = 1
+    return char_list
